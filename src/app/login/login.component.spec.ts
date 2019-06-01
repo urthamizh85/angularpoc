9597dff52e733a26b83eb692de33c29a05fa0ed1
urthamizh85 <urthamizh85@gmail.com>
2019-06-01 17:10:06 +0530

    1 file modified

--- src/app/login/login.component.spec.ts
+++ src/app/login/login.component.spec.ts
@@ -1,4 +1,4 @@
-import { async,  ComponentFixture,  TestBed,  fakeAsync,  flush } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, fakeAsync, flush } from '@angular/core/testing';
 import {
   BrowserDynamicTestingModule,
   platformBrowserDynamicTesting
@@ -7,13 +7,13 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { Authentication } from '../services/authentication';
-import { BrowserModule } from '@angular/platform-browser';
+import { BrowserModule, By } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
-import { LoginComponent } from './login.component';
+import { LoginComponent } from './login.component'
 import {
   MatInputModule,
   MatCardModule,
@@ -25,8 +25,9 @@ import {
 } from '@angular/material';
 
 describe('LoginComponent', () => {
-  let  fixture:  any;
-  beforeEach(async(()  =>  {
+  let fixture: any;
+
+  beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         BrowserModule,
@@ -43,20 +44,32 @@ describe('LoginComponent', () => {
         MatListModule,
         RouterTestingModule
       ],
-      declarations:  [LoginComponent],
+      declarations: [LoginComponent],
       providers: [Authentication]
     })
       .compileComponents();
   }));
-  beforeEach(()  =>  {
-    fixture  =  TestBed.createComponent(LoginComponent);
+  beforeEach(() => {
+    fixture = TestBed.createComponent(LoginComponent);
   })
 
-  it('Invalid User',  ()  =>  {
+  it('Login onsubmit', async(() => {
+    let fixture = TestBed.createComponent(LoginComponent);
     fixture.detectChanges();
-    console.log(fixture.componentInstance,fixture.debugElement);
-    expect(true).toBeTruthy();
-  });
+    spyOn(fixture.componentInstance, 'onsubmit');
+    fixture.whenStable().then(() => {
+      let username = fixture.debugElement.query(By.css('input[name="user"]'));
+      let pwd = fixture.debugElement.query(By.css('input[name="pwd"]'));
+      let usrel = username.nativeElement;
+      let pwdel = pwd.nativeElement;
+      usrel.value = 'Salman';
+      usrel.dispatchEvent(new Event('input'));
+      pwdel.value = 'Salman';
+      pwdel.dispatchEvent(new Event('input'));
+      fixture.debugElement.query(By.css('form')).triggerEventHandler('ngSubmit', null);
+      expect(fixture.componentInstance.onsubmit).toHaveBeenCalled();
+    });
+  }));
 
 
 })
